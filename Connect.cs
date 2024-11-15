51fdb6b41a37a14a7d4b53b24a19aa34b617d6e0
mark <piringerm@kkszki.hu>
2024-11-15 13:20:05 +0100

    select query is ready

---
+++ Connect.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace BasketTeam
+{
+    internal class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect() 
+        {
+            Host = "localhost";
+            Database = "team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+
+
+           Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZstdSharp.Unsafe;
+
+namespace BasketTeam
+{
+    internal class Program
+    {
+        public static Connect conn = new Connect();
+        public static void GetAllData() 
+        {
+           conn.Connection.Open();
+
+            string sql = "SELECT * FROM `player`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+            do 
+            {
+                var Player = new
+                {
+                    Id = dr.GetInt32(0),
+                    Name = dr.GetString(1),
+                    Height = dr.GetInt32(2),
+                    Weight = dr.GetInt32(3),
+                    CreatedTime = dr.GetDateTime(4),
+                };
+
+                Console.WriteLine($"Játékos adatok:{Player.Name}, {Player.Weight}, {Player.Height}");
+
+            } while (dr.Read());
+            
+            dr.Close();
+
+           
+
+           conn.Connection.Close();
+        }
+        static void Main(string[] args)
+        {
+            GetAllData();
+        }
+    }
+}
